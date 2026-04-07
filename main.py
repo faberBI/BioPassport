224e7d8d0151ce2f7a4fb2a10852889a7772fcd4
Fabrizio <fabriziodisciorio91@gmail.com>
2026-04-07 16:21:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -369,6 +369,16 @@ with tabs[2]:
         services.save_passport_to_file(passport)
         services.save_passport_to_excel_append(passport)
         st.session_state['published_passport'] = passport
+        # === GENERA PDF DEL PASSPORT ===
+        # === GENERA PDF UFFICIALE DEL DPP DALLA PAGINA PUBBLICA ===
+        public_url = f"{st.secrets['APP_URL']}?passport_id={passport['id']}"
+        pdf_bytes = services.generate_pdf_from_url(public_url)
+
+        passport["pdf_document"] = base64.b64encode(pdf_bytes).decode()
+
+        services.save_passport_to_file(passport)
+
+
 
         if qeseal_ok:
             st.success("✅ DPP pubblicato e sigillato (QeSeal)")
@@ -431,8 +441,9 @@ with tabs[2]:
         if submit_ses:
             with st.spinner("Invio richiesta di firma SES..."):
                 try:
+                    pdf_bytes = base64.b64decode(pp["pdf_document"])
                     services.sign_passport_pdf_ses_openapi(
-                        pp,
+                        pdf_bytes,
                         signer_name=st.session_state["ses_name"],
                         signer_surname=st.session_state["ses_surname"],
                         signer_email=st.session_state["ses_email"],
